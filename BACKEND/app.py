88aff8f3f6a993b7372b46d49901c042d86139df
AdhiNarayan206 <adhinarayan206@gmail.com>
2025-09-20 19:50:14 +0530

    ADDED requirement.txt & other files necessary for hosting the backend

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -4,6 +4,7 @@ import numpy as np
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
+import os
 
 # Load the saved model and scaler
 model = joblib.load(r"loan_approval_predicter\MODEL FILE\loan_approval_model.joblib")
@@ -41,4 +42,6 @@ def check_eligibility():
         return jsonify({'error': 'Invalid input format', 'message': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    port = int(os.environ.get('PORT', 5000))  # Use Render's PORT or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=False)
